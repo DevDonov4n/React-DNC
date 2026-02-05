d411c33f9eb314845153b1221efd873e5f703022
Donovan <donovanbueno456@gmail.com>
2026-02-05 13:56:50 -0300

    ADD: seção dos projetos(bem no inicio ainda)

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,16 @@
+import "../Projects/Projects.css"
+
+function Projects(){
+    return(
+        <>
+        <section className="projects-section">
+            <div className="projects-intro">
+                <h1 id='projects-title'>Follow Our Projects</h1>
+                <p id="projects-p">It is a long established fact that a reader will be distracted by the of readable content of page  lookings at its layouts  points.</p>
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default Projects
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import Header from "../components/Header/Header";
 import Banner from "../components/Banner/Banner";
+import Projects from "../components/Projects/Projects";
 
 function Home() {
     return (
         <>
             <Header/>
             <Banner/>
-
+            <Projects/>
         </>
     )
 }

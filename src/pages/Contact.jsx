b796e605e04b819b9a2d4897454d9141376f767b
Donovan <donovanbueno456@gmail.com>
2026-02-11 23:27:27 -0300

    Adicionando conteudo na pagina de contato

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,15 @@
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
+import "./Contact.css";
+
 function Contact() {
     return (
         <>
            <Header />
+           <section className="banner-contact">
+                <h1> Contact </h1>
+            </section>
            <Footer />
         </>
     )

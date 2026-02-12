9ea60f246325c2d1c9437c852355df0e77d4697c
DevDonov4n <donovanbueno456@gmail.com>
2026-02-12 10:12:15 -0300

    Adicionando o formulário da página de contato

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,13 +6,38 @@ import "./Contact.css";
 function Contact() {
     return (
         <>
-           <Header />
-           <section className="banner-contact">
-                <h1> Contact </h1>
+            <Header />
+
+            <section className="banner-contact">
+                <h1>Contact</h1>
             </section>
-           <Footer />
+
+            <h1 id="p-contact">We love meeting new people and helping them.</h1>
+
+            <form>
+                <div className="row">
+                    <div className="input-group">
+                        <label>Name *</label>
+                        <input type="text" required />
+                    </div>
+
+                    <div className="input-group">
+                        <label>Email *</label>
+                        <input type="email" required />
+                    </div>
+                </div>
+
+                <div className="input-group">
+                    <label>Hello Iam Intrested in..</label>
+                    <textarea rows="4" />
+                </div>
+
+                <button id="button-contact" type="submit">Send Now →</button>
+            </form>
+
+            <Footer />
         </>
-    )
+    );
 }
 
 export default Contact;
